30412590c7e43d4e4ff26d90add36144e36014d8
Dominika <dominika.muz@gmail.com>
2016-04-28 00:23:58 +0200

    interview gui

---
+++ app/src/main/java/com/example/dominika/appobchod/Alergy.java
@@ -0,0 +1,13 @@
+package com.example.dominika.appobchod;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Alergy extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alergy);
+    }
+}

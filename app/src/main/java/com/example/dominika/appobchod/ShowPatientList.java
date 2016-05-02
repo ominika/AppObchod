2a69d7147ca35f88967c50c9fb100c892c6c4cf9
beszczur <beszczur@gmail.com>
2016-05-02 14:28:43 +0200

    New ability - findingPatients by Name

--- app/src/main/java/com/example/dominika/appobchod/ShowPatientList.java
+++ app/src/main/java/com/example/dominika/appobchod/ShowPatientList.java
@@ -5,40 +5,76 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class ShowPatientList extends AppCompatActivity {
 
     private ListView list;
+    String[] items;
     ArrayList<String> patients = new ArrayList<String>();
+    EditText editText;
+    PatientListViewItemAdapter adapter;
+    int editTextLastLength;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_patient_list);
         list = (ListView) findViewById(R.id.listView);
+        editText = (EditText)findViewById(R.id.findPatientTextEdit);
+        editTextLastLength = 0;
 
+        initList();
 
-        patients.add("Dominika Muzyka");
-        patients.add("Beata Szturemska");
-        patients.add("Jan Nowak");
-        patients.add("Agnieszka Krzemińska");
-        patients.add("Paweł Chodzieski");
-        patients.add("Jędrzej Gołębiewski");
-        patients.add("Agata Kowalska");
-        patients.add("Piotr Ratajczak");
-        patients.add("Albert Einstein");
-        patients.add("Patryk Gliszczyński");
-        patients.add("Małgorzata Janicka");
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if(s.toString().equals(""))
+                    initList();
+                else
+                    searchItem(s.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+    }
+
+    public void initList()
+    {
+        items = new String[]{"Dominika Muzyka",     "Beata Szturemska",     "Jan Nowak",        "Patryk Gliszczyński",      "Małgorzata Janicka"};
+        patients = new ArrayList<>(Arrays.asList(items));
         //for(int i=0; i<9; i++)
-            //data.add(new PatientListViewItem(i,cars[i]));
+        //data.add(new PatientListViewItem(i,cars[i]));
+        adapter = new PatientListViewItemAdapter(this, R.layout.patient_listview_item, patients);
+        list.setAdapter(adapter);
+    }
 
-        list.setAdapter(new PatientListViewItemAdapter(this, R.layout.patient_listview_item, patients));
+    public void searchItem(String textToSearch){
+        if(editTextLastLength > textToSearch.length())
+            initList();
+        editTextLastLength = textToSearch.length();
+        for(String item:items){
+            if(!item.contains(textToSearch))
+                patients.remove(item);
+        }
+
+        adapter.notifyDataSetChanged();
     }
 }

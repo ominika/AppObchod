eed694f98576e24303db73606fa9b53db3d776e8
beszczur <beszczur@gmail.com>
2016-05-02 13:56:30 +0200

    InvestigationWindow was created

--- app/src/main/java/com/example/dominika/appobchod/Diagnostic.java
+++ app/src/main/java/com/example/dominika/appobchod/Diagnostic.java
@@ -2,6 +2,9 @@ package com.example.dominika.appobchod;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Switch;
 
 public class Diagnostic extends AppCompatActivity {
 
@@ -9,5 +12,19 @@ public class Diagnostic extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_diagnostic);
+
+        Button setInvestigationButton = (Button) findViewById(R.id.SetInvestigationButton);
+        assert setInvestigationButton != null;
+        setInvestigationButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                setInvestigationOnClick();
+            }
+        });
+    }
+
+    public void setInvestigationOnClick(){
+        Switch mySwitch = (Switch) findViewById(R.id.CITOSwitch);
+        mySwitch.setChecked(false);
+        //TODO: Add Investigation to database
     }
 }
